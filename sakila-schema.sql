44ad3d8467d1843353b044e6938babebf7e20b6a
Linuxdict <edy@linuxdict.com>
2015-07-15 11:04:16 +0800

    Remove the fk, seems doesn't fit the populate sql.
    mysql> call populate('sakila','film',100,'N');
    ERROR 1452 (23000): Cannot add or update a child row: a foreign key constraint fails (`sakila`.`film`, CONSTRAINT `fk_film_language` FOREIGN KEY (`language_id`) REFERENCES `language` (`language_id`) ON UPDATE CASCADE)

--- sakila-schema.sql
+++ sakila-schema.sql
@@ -132,8 +132,6 @@ CREATE TABLE film (
   KEY idx_title (title),
   KEY idx_fk_language_id (language_id),
   KEY idx_fk_original_language_id (original_language_id),
-  CONSTRAINT fk_film_language FOREIGN KEY (language_id) REFERENCES language (language_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_film_language_original FOREIGN KEY (original_language_id) REFERENCES language (language_id) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
